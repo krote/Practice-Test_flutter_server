72385ac0e666bb9599687c149c27c6c530c1fb23
krote <krote0924@gmail.com>
2023-07-15 21:56:41 +0900

    create server project

---
+++ bin/test_flutter_server.dart
@@ -0,0 +1,5 @@
+import 'package:test_flutter_server/test_flutter_server.dart' as test_flutter_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_flutter_server.calculate()}!');
+}
---
+++ test/test_flutter_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_flutter_server/test_flutter_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
